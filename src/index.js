922783c878596435bc4b09fcf6ccc1578fcda284
midas@tower-windows <javierlianorioz@alumnos.uneatlantico.es>
2025-03-04 17:24:58 +0100

    Enhance QR code functionality with clipboard support and update styles for improved UI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-undef */
 const qrData = document.getElementById('qr-input');
 const qrButton = document.getElementById('qr-button');
+const pasteButton = document.getElementById('paste-button');
+const domainDisplay = document.getElementById('domain-display');
 const downloadLink = document.getElementById('a-download');
 
 const qrcode = new QRCode(document.getElementById('qrcode'), {
   text: '',
   width: 280,
   height: 280,
-  colorDark: '#551A8B',
-  colorLight: '#F8F8F8',
+  colorDark: '#8aadf4', // primary color
+  colorLight: '#24273a', // background color
   correctLevel: QRCode.CorrectLevel.H,
 });
 
@@ -20,6 +22,7 @@ window.addEventListener('load', () => {
     const currentUrl = tabs[0].url;
     qrcode.makeCode(currentUrl);
     qrData.value = currentUrl;
+    domainDisplay.value = new URL(currentUrl).hostname;
     sleep(100).then(() => {
       const src = document.querySelector('img').getAttribute('src');
       document.getElementById('a-download').href = `${src}`;
@@ -30,11 +33,22 @@ window.addEventListener('load', () => {
 
 qrButton.addEventListener('click', () => {
   qrcode.makeCode(qrData.value);
+  domainDisplay.value = new URL(qrData.value).hostname;
   sleep(100).then(() => {
     const src = document.querySelector('img').getAttribute('src');
     document.getElementById('a-download').href = `${src}`;
   });
   downloadLink.download = 'qrcode';
+});
+
+pasteButton.addEventListener('click', async () => {
+  try {
+    const text = await navigator.clipboard.readText();
+    qrData.value = text;
+    domainDisplay.value = new URL(text).hostname;
+  } catch (err) {
+    console.error('Failed to read clipboard contents: ', err);
+  }
 });
 
 qrData.addEventListener('input', function() {
